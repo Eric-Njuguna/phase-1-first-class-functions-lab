381d9aa4db31785d0b6ff609db5df195d5f65b84
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-03 08:13:33 +0300

    Adds first class functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => {
+    let copyofDrivers = [...drivers];
+    return copyofDrivers.splice(0, 2);
+};
+
+const returnLastTwoDrivers = () => {
+    let copyofDrivers = [...drivers];
+    return copyofDrivers.splice(2);
+};
+
+// console.log(returnFirstTwoDrivers(drivers));
+// console.log(returnLastTwoDrivers(drivers));
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//console.log(selectingDrivers[0]);
+// console.log(selectingDrivers[1](drivers));
+
+function createFareMultiplier(integer) {
+    const fareMultiplier = () => integer * integer;
+    return fareMultiplier;
+}
+
+console.log(createFareMultiplier(2));
+
+const fareDoubler = (fare) => fare * 2;
+
+console.log(fareDoubler(10));
+
+const fareTripler = (fare) => fare * 3;
+
+console.log(fareTripler(12));
+
+
+function selectDifferentDrivers(drivers, callBack) {
+    return callBack(drivers);
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
